e7a0f68d721950ad12fa1c75e1925e3ecbae9792
Shnyamsky <ivanov.ilya-iv@yandex.ru>
2022-09-13 22:13:29 +0500

    task-3 solutions

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const keys = path.split('.');
+  
+  return function(obj) {
+    let result = obj;
 
+    for (let value of keys) {
+      result = result[value];
+      if (result === undefined) break;
+    }
+
+    return result;
+  }
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,23 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  
+  if (size === undefined) return string;
 
+  let result = '';
+  let counter = 0;
+  let currentSymbol = string[0];
+
+  for (let symbol of string) {
+    if (symbol === currentSymbol) {
+      counter++;
+      counter <= size ? result += symbol : result;
+    } else {
+      currentSymbol = symbol;
+      counter = 1;
+      counter <= size ? result += symbol : result;
+    }
+  }
+
+  return result;
 }
